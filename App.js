d6db7db4bc79c030ef285615e2b1dd10ff2a25f0
Sourabh <sourabh999pal@gmail.com>
2022-11-22 16:01:14 +0530

    this is first branch to test something 2

--- App.js
+++ App.js
@@ -1,22 +1,78 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 
 
 import { NavigationContainer } from '@react-navigation/native';
 
 import Tabnavigation from './src/navigation/Tabnavigation';
-import Home from './src/screens/Home';
+
+//introapp imports here 
+import AppIntroSlider from 'react-native-app-intro-slider';
+
+
+
 
 
 const App = () => {
-  return (
-  <NavigationContainer>
+
+  const [realshow , setRealshow] = useState(false);
+  
+  const On_done =()=>{
+    setRealshow(false);
+  }
+
+  const slides = [
+    {
+      key: 1,
+      title: 'Title 1',
+      text: 'Description.\nSay something cool',
+      image: require('./src/asource/1.jpg'),
+      backgroundColor: '#59b2ab',
+    },
+    {
+      key: 2,
+      title: 'Title 2',
+      text: 'Other cool stuff',
+      image: require('./src/asource/1.jpg'),
+      backgroundColor: '#febe29',
+    },
+    {
+      key: 3,
+      title: 'Rocket guy',
+      text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
+      image: require('./src/asource/1.jpg'),
+      backgroundColor: '#22bcb5',
+    }
+  ];
+
+  const renderSlide = ({item})=>{
+     return (
+      <View>
+        <Text>{item.title}</Text>
+      </View>
+     )
+  }
+
+  return(
+   <ScrollView>
+    <View>
+      {realshow ? <AppIntroSlider /> : <Main/>  }
+    </View>
+   </ScrollView>
+  )
+}
+
+export default App
+
+const Main = () => {
+  return(
+    <NavigationContainer>
     <Tabnavigation/>
   
   </NavigationContainer>
   )
 }
 
-export default App
+
 
 const styles = StyleSheet.create({})
